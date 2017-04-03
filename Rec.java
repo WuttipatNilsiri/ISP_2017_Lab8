77c995a2a7259e1b9b2249bb26316ee9fe3445c4
Poorin Pichayamongkol <poorinsmoothy@hotmail.com>
2017-04-03 15:33:00 +0700

    commit method reverse

--- Rec.java
+++ Rec.java
@@ -1,3 +1,5 @@
+package isp;
+
 public class Rec {
 
 
@@ -33,8 +35,12 @@ public class Rec {
 		Example: reverse("abcdefg") = "gfedcba". */
 	public static String reverse(String s) {
 	
-	// fill me in
+	String result = "" ;
 	
+	for ( int i = s.length()-1 ; i>=0 ; i-- ) {
+		result = result + s.charAt(i) ;
+	}
+	return result ;
 	}
 
     public static void main(String[] args) {
